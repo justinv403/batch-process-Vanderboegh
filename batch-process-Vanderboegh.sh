880e41b694d21460175b6a8722cec7b575060160
Justin <79169357+justinv403@users.noreply.github.com>
2023-09-29 04:32:34 +0000

    Initial Commit
    Added batch-process-Vanderboegh.sh, which displays file directory, size, and number of words in that file.

---
+++ batch-process-Vanderboegh.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    WORDS="$(wc -w "${f}" | cut -d' ' -f1)"
+
+    echo "Processing $f file..."
+    echo "Size: $SIZE"
+    echo "Word Count: $WORDS"
+  fi
+done
